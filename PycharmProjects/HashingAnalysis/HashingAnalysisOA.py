431a0fa678b944dc85e4e24c9a71d94875cdd14f
Ank <a.vishnubhotla@ufl.edu>
2018-10-27 13:23:45 -0400

    OA table working

--- PycharmProjects/HashingAnalysis/HashingAnalysisOA.py
+++ PycharmProjects/HashingAnalysis/HashingAnalysisOA.py
@@ -1,17 +1,51 @@
-class hashTable:
-    hashTable = [0] * 10;
-    collisions = 0;
-    def computehash(self, x, hashTable):
-        return x % len(hashTable)
+import random
+class HashTableOA(object):
+    hashTable = [""] * 100
+    collisions = 0
+    numElements = 0
 
-    def insertkey(self, hashTable, key, value, collisions):
-        index = computehash(x, hashTable)
-        if hashTable[index] == 0:
+    def __init__(self, size):
+        self.hashTable = [-1] * size
+        self.collisions = 0
+        self.numElements = 0
+
+    def insertkey(self, index, value):
+        # if self.filled():
+        #     return
+        if self.hashTable[index] != -1:
             start = 0
-            collisions += 1
-            while hashTable[start] == 0:
+            self.collisions += 1
+            while self.hashTable[start] != -1:
                 start += 1
-            hashTable[start] = value
+                self.collisions += 1
+            self.hashTable[start] = value
         else:
-            hashTable[index] == value
-        return collisions
+            self.hashTable[index] = value
+        self.numElements += 1
+
+    def computehashmod(self, num):
+        return num % len(self.hashTable)
+
+    # def filled(self):
+    #     if floor(self.numElements / len(self.hashTable)) == self.loadFactor:
+    #         return True
+    #     else:
+    #         return False
+
+
+def computehashmsq(self, num):
+    num *= num
+
+
+hashOAmod = HashTableOA(100)
+value = "hello"
+
+for i in hashOAmod.hashTable:
+    randNum = random.randint(0, 300)
+    key = hashOAmod.computehashmod(randNum)
+    hashOAmod.insertkey(key, value)
+    if hashOAmod.numElements % 10 == 0:
+        print("load factor: ", hashOAmod.numElements / 100, ", # of collisions: ", hashOAmod.collisions)
+
+
+
